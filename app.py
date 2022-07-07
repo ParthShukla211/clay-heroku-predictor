27a2a21f0a8839a66df6ac32e43a76cccece26d6
Parth Shukla <shuklaparth501@gmail.com>
2022-07-07 11:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,15 +2,15 @@ import streamlit as st
 import pickle
 import pandas as pd
 import numpy as np
+from catboost import CatBoostRegressor
 
 
-pipe = pickle.load(open('model.pkl','rb'))
+
+pipe = pickle.load(open('cat_model.pkl','rb'))
 st.title('Clay Predictor')
 
 
 Coarse = st.number_input('Coarse')
-Sand = st.number_input('Sand')
-Silt = st.number_input('Silt')
 cacl2ph = st.number_input('cacl2ph')
 h2oph = st.number_input('h2oph')
 EC = st.number_input('EC')
@@ -19,11 +19,12 @@ CaCO3 = st.number_input('CaCO3')
 P = st.number_input('P')
 N = st.number_input('N')
 K = st.number_input('K')
+Soil_Stones=st.number_input('Soil_Stones')
 
 
 # 'pH(CaCl2)':[pH(CaCl2)],'pH(H2O)':[pH(H2O)],
 if st.button('Predict Clay'):
-      input=pd.DataFrame({'Coarse':[Coarse],'Sand':[Sand],'Silt':[Silt],'cacl2ph':[cacl2ph],'h2oph':[h2oph],'EC':[EC],'OC':[OC],'CaCO3':[CaCO3],'P':[P],'N':[N],'K':[K]})
+      input=pd.DataFrame({'Coarse':[Coarse],'cacl2ph':[cacl2ph],'h2oph':[h2oph],'EC':[EC],'OC':[OC],'CaCO3':[CaCO3],'P':[P],'N':[N],'K':[K],'Soil_Stones':[Soil_Stones]})
       result = pipe.predict(input)
       st.success('THE Clay FOR GIVEN DATA WILL BE {}'.format(result))
 # st.header(result)
